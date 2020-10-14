aeb9876edbb00a359891f0f2c47a8716c2f2253e
Yonicel Leyva <yonicel@yonicels-mbp.lan>
2020-10-13 22:40:22 -0400

    update readme

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -16,9 +16,6 @@ const useStyles = makeStyles((theme) => ({
 function Topbar({emailsSelected, onDeleteEmail}) {
   const classes = useStyles();
 
-  useEffect(() => {
-    console.log(emailsSelected)
-  }, [emailsSelected])
   return (
     <>
       <AppBar position="fixed" className={classes.appBar}>
