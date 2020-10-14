24235c0f271bab48193cd0e551404396380f246e
Yonicel Leyva <yonicel@yonicels-mbp.lan>
2020-10-13 22:23:12 -0400

    Updates

---
+++ precache-manifest.4306b05b792a28524ee3334eacba78e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a02f8c8ad2fbb33339bf1d6a078fd7e",
+    "url": "/ui-excercise/index.html"
+  },
+  {
+    "revision": "3d6261d2aea69cec3085",
+    "url": "/ui-excercise/static/css/main.60f9e1d7.chunk.css"
+  },
+  {
+    "revision": "88383417e20a7444ed86",
+    "url": "/ui-excercise/static/js/2.6433298f.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/ui-excercise/static/js/2.6433298f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d6261d2aea69cec3085",
+    "url": "/ui-excercise/static/js/main.815f42af.chunk.js"
+  },
+  {
+    "revision": "06a06d87350c1cb324bd",
+    "url": "/ui-excercise/static/js/runtime-main.972d8d9a.js"
+  }
+]);
